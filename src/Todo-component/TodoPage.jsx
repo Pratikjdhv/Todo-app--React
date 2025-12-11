d2def0a9dd976563516e1cb232de4b53344cf6dc
Pratik Jadhav <pj2968156@gmail.com>
2025-12-11 18:13:23 +0530

    Add features - sort , edit , update , move up , move down

--- src/Todo-component/TodoPage.jsx
+++ src/Todo-component/TodoPage.jsx
@@ -38,14 +38,59 @@ const TodoPage = () => {
     setTodos (newtodos);
 }
 
-    function ontododelete (id){
+    function handledeleteTodo (id){
         const newtodos = todos.filter(item => item.id != id )
         setTodos (newtodos);
     }
 
     const emptystate = <h3>Nothing's Here , Add a ToDo </h3>
 
-  return (
+    const completedtodos= todos.filter(item => item.completed).length ;
+
+    const istodoEmpty = todos.length == 0 
+    
+    const comparefunction = (a,b) => a.text.localeCompare (b.text) ;
+
+    const istodosorted = todos.every((todo , index , arr ) =>{
+        return index === 0 || comparefunction(arr[index-1], todo) <= 0 ;
+    })
+
+    function handledelete() {
+        setTodos ([]) ;
+    }
+
+    function handlesorttodos() {
+        const newtodos = [...todos] ;
+        newtodos.sort(comparefunction) ;
+        setTodos (newtodos) ;
+    }
+
+    function handleUpdateTodoText (id , todotext ){
+        const newtodos = todos.map((item) => {
+            if (item.id === id ){
+                return {...todos , text: todotext }
+            }
+            return item 
+        })
+        setTodos(newtodos)
+    }
+
+    function handleMoveUp (index){
+        if(index == 0) return ;
+        const newtodos = [...todos];
+        [newtodos[index] , newtodos[index -1 ]] = [newtodos[index-1] , newtodos[index]]
+        setTodos(newtodos)
+    }
+    function handleMoveDown (index){
+        if(index == todos.length - 1 ) return ;
+        const newtodos = [...todos];
+        [newtodos[index] , newtodos[index +1 ]] = [newtodos[index+1] , newtodos[index]]
+        setTodos(newtodos)
+
+    }
+
+
+    return (
     <div>
         <h1>Super To-Do</h1>
         <form onSubmit={handlesubmit}>
@@ -57,11 +102,24 @@ const TodoPage = () => {
             <button>Submit</button>
         </form>
 
-        {todos.length > 0 ? <div>
-            {todos.map((item) => (
+        {!istodosorted &&
+            <button onClick={handlesorttodos}>Sort ToDos</button>}
+        {!istodoEmpty &&
+            <button onClick={handledelete}>Delete All </button>}
+        {!istodoEmpty &&
+            <p>{completedtodos}/{todos.length} completed </p>}
+
+        {!istodoEmpty ? 
+        <div>
+            {todos.map((item , index) => (
                 <Todoitem key={item.id} item={item} 
                 handletoggle={handletoggle}
-                ontododelete = {ontododelete}
+                ontododelete = {handledeleteTodo}
+                OnTodoEditUpdate = {handleUpdateTodoText}
+                onMoveUp={handleMoveUp}
+                onMoveDown={handleMoveDown}
+                index={index}
+                todosCount={todos.length}
                 />
             ))}
         </div> 
--- src/Todo-component/Todoitem.jsx
+++ src/Todo-component/Todoitem.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Todoitem = ({item , handletoggle ,ontododelete }) => {
+const Todoitem = ({item , handletoggle ,ontododelete  , OnTodoEditUpdate , onMoveUp , onMoveDown ,index ,todosCount }) => {
 
+  const [showedittodo , setedittodo ] = useState(false);
 
-  return (
+  function handleEditTodo (e){
+    e.preventDefault ()
+    const todoText = e.target['todo'].value;
+    OnTodoEditUpdate (item.id , todoText);
+    setedittodo(false)
+  }
+
+  const todoEditForm =(
     <div>
+      <form onSubmit={handleEditTodo} >
+        <input type="text" name="todo" defaultValue={item.text}/>
+        <button >
+          Update 
+        </button>
+        <button onClick={() => setedittodo (false)} >
+          Cancle 
+        </button>
+      </form>
+    </div>
+  );
+
+  const todoitem = (
+    <div>
+        <button disabled={index==0}onClick={() => onMoveUp(index)}>⬆️</button>
+        <button disabled={index==todosCount-1}onClick={() => onMoveDown(index)}>⬇️</button>
         <input 
         id = {item.id}
         checked={item.completed}
@@ -15,9 +39,18 @@ const Todoitem = ({item , handletoggle ,ontododelete }) => {
         style={{textDecoration : item.completed ? 'line-through' : 'none'}}>
         {item.text}</label>
 
+        <button onClick={() => setedittodo(true)}>Edit</button>
         <button onClick={() => ontododelete(item.id)}>
             Delete
         </button>
+    </div>
+  )
+
+
+
+  return (
+    <div>
+      {showedittodo ? todoEditForm : todoitem}
     </div>
   )
 }
